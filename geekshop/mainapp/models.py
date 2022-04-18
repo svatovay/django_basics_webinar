d5e89c1b48b4f2112a0e2a6692e1c6a34004a649
alexeysvatov <zombeaver@gmail.com>
2022-04-18 21:46:42 +0500

    need to done task_2

---
+++ geekshop/mainapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class ProductCategory(models.Model):
+    name = models.CharField(verbose_name='название категории', max_length=64, unique=True)
+    description = models.TextField(verbose_name='описание', blank=True)
+
+    def __str__(self):
+        return f'{self.id}. {self.name}'
